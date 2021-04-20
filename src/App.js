dc41338f17fd861e9e01e401633248b749823192
Voikyrioh <voikyrioh@gmail.com>
2021-04-20 23:22:40 +0200

    [feat] App page will now fetch api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+    componentDidMount() {
+        fetch("http://localhost:8080/sources/", 
+            {
+                headers: {
+                    "access-control-allow-origin" : "*",
+                    "Content-type": "application/json; charset=UTF-8"
+                }})
+            .then(response => response.json())
+            .then(reponse => {
+                this.setState({
+                    isLoaded: true,
+                    items: reponse
+                });
+            },(error) => {
+                this.setState({
+                    isLoaded: true,
+                    error
+                });
+        })
+    }
+    
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Erreur : {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Chargement…</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            {item.name} {item.status}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
 }
 
 export default App;
