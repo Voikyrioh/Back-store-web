ea5052457c7c4fc07f2e72dbafd42c6cb62a1a97
Voikyrioh <voikyrioh@gmail.com>
2021-11-06 17:19:40 +0100

    [feat] Added comments for input phone

--- src/components/Inputs/InputPhone.js
+++ src/components/Inputs/InputPhone.js
@@ -1,58 +1,69 @@
+//#region imports
+/* REACT IMPORTS */
 import * as React from 'react';
-import {Avatar, Form, Input, Select} from "antd";
+import { useState } from "react";
+
+/* ANTD IMPORTS */
+import { Avatar, Form, Input, Select } from "antd";
+
+/* External libraries imports */
 import { countries } from 'country-flag-icons';
+import Flags from 'country-flag-icons/react/1x1';
 import {
     getCountryCallingCode,
     isSupportedCountry,
     isValidNumberForRegion,
     parsePhoneNumber
 } from 'libphonenumber-js';
-import Flags from 'country-flag-icons/react/1x1';
-import {useState} from "react";
-const { Option } = Select;
+//#endregion
 
 export function InputPhone(props) {
     const [selectedCountry, setSelectedCountry] = useState(props.defaultCountry);
     const [inputStatus, setInputStatus] = useState('');
 
-    // #region Utility functions
-    const getCountryDialOptions = () => {
+    //#region Utility functions
+    // create a select's option for all countries
+    function getCountryDialOptions() {
         const options = [];
         const availableCountries = countries.filter(value => isSupportedCountry(value)).sort((a,b) => getCountryCallingCode(a) - getCountryCallingCode(b));
         for (const country of availableCountries) {
             const Flag = Flags[country];
             options.push((
-                <Option value={country} key={country}>
+                <Select value={country} key={country}>
                     <Avatar size={15} icon={<Flag/>}/> <span style={{verticalAlign: 'middle', marginLeft: 5}}>+{getCountryCallingCode(country)}</span>
-                </Option>
+                </Select>
             ))
         }
         return options;
     }
-    const changeCountrySelection = (value, options) => {
+
+    // Event to trigger when country's selection is changed
+    function changeCountrySelection(value, options) {
         setSelectedCountry(value);
         props.form.validateFields([props.name]).then(value1 => {})
     }
 
-    const filterCountrySearch = (value, option) => {
+    // Event to trigger when a search filter is called
+    function filterCountrySearch(value, option) {
         const countryCallingCode = `+${getCountryCallingCode(option.value)}`;
         return option?.value?.includes(value?.toUpperCase()) || countryCallingCode?.includes(value);
     }
 
-    const checkPhoneValid = (phone) => {
+    // Phone validation function
+    function checkPhoneValid(phone) {
         return isValidNumberForRegion(phone, selectedCountry);
     }
 
-    const computeValue = (phone) => {
+    // Function called when form is submitted to format this field output
+    function computeValue(phone) {
         if (isValidNumberForRegion(phone, selectedCountry) && phone.length > 1 && typeof phone === 'string') {
             const number = parsePhoneNumber(`${phone}`, selectedCountry);
             return number.formatInternational();
         } else {
             return phone;
         }
-
     }
-    // #endregion
+    //#endregion
 
     const countrySelect = (
         <Select showSearch
